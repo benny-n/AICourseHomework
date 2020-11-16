73a1c393b3c141df86f6461f0ae30a9682ebf183
benny-n <benick12@gmail.com>
2020-11-16 14:44:28 +0200

    deleted 3 lines of print

--- ai_hw1/main.py
+++ ai_hw1/main.py
@@ -107,9 +107,6 @@ def run_astar_for_weights_in_range(heuristic_type: HeuristicFunctionType, proble
 
 def toy_map_problem_experiments():
     print('Solve the map problem.')
-    print('check that git collab works as intended')
-    print('check again?')
-    print('check with git desktop')
 
     # Ex.10
     # TODO: Just run it and inspect the printed result.
